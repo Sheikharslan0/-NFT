01871afe52c98f33aa248059e92a00363be0213c
Sheikharslan0 <sa5251559@email.com>
2024-11-13 12:29:18 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NFT/', // yahan 'NFT' aapke GitHub repository ka name hona chahiye
+  base: '/', // yahan 'NFT' aapke GitHub repository ka name hona chahiye
 });
